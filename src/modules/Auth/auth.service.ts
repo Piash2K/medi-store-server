8d69a4033f418a57bbb9268404b69e16d6285845
Piash Islam <piashislam2k@gmail.com>
2026-02-19 21:38:14 +0600

    password hashed method added done

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,10 +1,18 @@
 import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
 
 const createUserIntoDB = async (payload: any) => {
+
+    const  hashPassword = await bcrypt.hash(payload.password, 8);
+
     const result = await prisma.user.create({
-        data: payload
+        data: {
+            ...payload,
+            password: hashPassword
+        }
     });
-    return result;
+    const { password, ...rest } = result;
+    return rest;
 };
 
 export const AuthService = {
